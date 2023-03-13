0a6f306fccbea4be8c2ef626a89f534b28ca124d
Anshul patle <111644282+Anshul-0807@users.noreply.github.com>
2023-03-13 23:31:42 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
-function calculation(num1, num2){
-    return num1 + num2;
+////////////////-----------------LEARN TYPESCRIPT-------------------/////////////////////
+// var transaction1 = {
+//     payerAccountNumber: 344,
+//     payeeAccountNumber: 788
+// };
+// var transaction2 = {
+//     payerAccountNumber: 344,
+//     payeeAccountNumber: 788
+// };
+// var bankAccount = {
+//     accountNumber: 88283,
+//     accountHolder: 'jonhy depp',
+//     balance: 73,
+//     isActive: true,
+//     transaction: [transaction1, transaction2]
+// };
+// var book = {
+//     name: ' A great Leader ',
+//     price: 1300,
+//     fileSize: ' 300MB',
+//     formated: 'pdf',
+//     duration: 4
+// };
+
+
+
+
+
+function printId(id) {
+    if (typeof id === 'string') {
+        console.log(id.toUpperCase());
+    }
+    else {
+        console.log(id);
+    }
 }
-console.log(calculation(5, 7));
+
